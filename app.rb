5bd56da58a812433389f4968934222ca12e5fd60
James Benjamin <james.abenj@gmail.com>
2020-03-12 22:05:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ class App < Sinatra::Base
     @number = params[:number].to_i
     @phrase = params[:phrase]
     array = @number.times.map{|i| i = @phrase}
-    "#{array.join(" ")}"
+    "#{array.join("/n")}"
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
