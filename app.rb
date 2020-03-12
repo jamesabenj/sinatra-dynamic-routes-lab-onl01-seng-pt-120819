00e60eb140049e7789a41feca5c462b69dd5131e
James Benjamin <james.abenj@gmail.com>
2020-03-12 20:57:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -31,5 +31,6 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1 = params[:number1]
     @number2 = params[:number2]
+    "#{@number1}#{@operation}#{@number2}"
   end
 end
