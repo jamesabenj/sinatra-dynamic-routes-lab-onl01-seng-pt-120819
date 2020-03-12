3329e780b777377bbc6211834d4238f7cc2babde
James Benjamin <james.abenj@gmail.com>
2020-03-12 21:02:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -39,6 +39,6 @@ class App < Sinatra::Base
     end
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
-    "#{@number1}#{@operation}#{@number2}"
+    "#{@number1 @operation @number2}"
   end
 end
